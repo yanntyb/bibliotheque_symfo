4eaf531a85b943a521bf70a5438757ba4e015a66
yanntyb <yanntyb.lbc@gmail.com>
2022-02-25 13:36:24 +0100

    fini

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -11,24 +11,38 @@ use App\Repository\ShelfRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("/book", "book_")]
 class BookController extends AbstractController
 {
+
+    private BookRepository $bookR;
+    private ClientRepository $clientR;
+    private CategoryRepository $categoryR;
+    private EntityManagerInterface $em;
+
+    public function __construct(BookRepository $bookRepository, CategoryRepository $categoryRepository, ClientRepository $clientRepository, EntityManagerInterface $em){
+        $this->bookR = $bookRepository;
+        $this->clientR = $clientRepository;
+        $this->categoryR = $categoryRepository;
+        $this->em = $em;
+    }
+
     #[Route("/add", name: "add")]
-    public function add(CategoryRepository $categoryRepository, BookRepository $bookRepository, EntityManagerInterface $em): Response
+    public function add(): Response
     {
         $book = new Book();
         $book
-            ->setTitle("book-" . $bookRepository->getLatestId())
-            ->setCategory($categoryRepository->getRandom())
+            ->setTitle("book-" . $this->bookR->getLatestId())
+            ->setCategory($this->categoryR->getRandom())
             ->setAvailable(true)
             ->setClient(null);
 
-        $em->persist($book);
-        $em->flush();
+        $this->em->persist($book);
+        $this->em->flush();
 
         return $this->render('book/index.html.twig', [
             'book' => $book,
@@ -37,19 +51,19 @@ class BookController extends AbstractController
     }
 
     #[Route("/take", name: "take")]
-    public function take(BookRepository $repository, ClientRepository $clientRepository, EntityManagerInterface $em, ShelfRepository $shelfRepository): JsonResponse
+    public function take(ShelfRepository $shelfRepository): JsonResponse
     {
-        $client = $clientRepository->find(1);
+        $client = $this->clientR->find(1);
         $data = json_decode(file_get_contents("php://input"));
         $id = $data->id;
-        $book = $repository->find($id);
+        $book = $this->bookR->find($id);
         $book
             ->setAvailable(false)
             ->setClient($client);
 
         $client->addBook($book);
 
-        $em->flush();
+        $this->em->flush();
 
         $shelfs = $shelfRepository->findAll();
         $data = [];
@@ -99,8 +113,9 @@ class BookController extends AbstractController
     /**
      * @var Client $client
      */
-    public function rendre(BookRepository $repository, ClientRepository $clientRepository, EntityManagerInterface $em){
-        $client = $clientRepository->find(1);
+    public function rendre(): RedirectResponse
+    {
+        $client = $this->clientR->find(1);
         /**
          * @var Book $book
          */
@@ -108,7 +123,7 @@ class BookController extends AbstractController
             $book->setAvailable(true);
             $client->removeBook($book);
         }
-        $em->flush();
+        $this->em->flush();
 
         return $this->redirectToRoute("shelf_all");
     }
